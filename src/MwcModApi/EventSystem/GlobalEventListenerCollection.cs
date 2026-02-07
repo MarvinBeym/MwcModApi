a78c7158cb623dac3a7c25e49932ba4c6aabee32
Marvin Beym <Marvin.Beym@gmx.de>
2026-02-07 19:19:41 +0100

    Implement ability to register global event listeners (Example: engine running, engine stalls)

---
+++ src/MwcModApi/EventSystem/GlobalEventListenerCollection.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MwcModApi.EventSystem
+{
+	/// <summary>
+	/// Collection of EventListener objects
+	/// </summary>
+	internal class GlobalEventListenerCollection
+	{
+		/// <summary>
+		/// Flag set when executing InvokeAll method,
+		/// if set to true, EventListener removed using the Remove method will not be executed in the InvokeAll and removed after the InvokeAll is finished
+		/// </summary>
+		private bool currentlyIterating;
+
+		/// <summary>
+		/// Protected writable list of EventListener
+		/// </summary>
+		private List<GlobalEventListener> _eventListeners;
+
+		/// <summary>
+		/// A collection of EventListener
+		/// </summary>
+		/// <param name="eventListeners">Initial list of EventListener</param>
+		internal GlobalEventListenerCollection(List<GlobalEventListener> eventListeners)
+		{
+			_eventListeners = eventListeners;
+		}
+
+		/// <summary>
+		/// A collection of EventListener
+		/// </summary>
+		internal GlobalEventListenerCollection()
+		{
+			_eventListeners = new List<GlobalEventListener>();
+		}
+
+		/// <summary>
+		/// Adds a new EventListener to this collection
+		/// </summary>
+		/// <param name="partEventListener">The EventListener object to add to the collection</param>
+		internal void Add(GlobalEventListener partEventListener)
+		{
+			_eventListeners.Add(partEventListener);
+		}
+
+		/// <summary>
+		/// Invokes all EventListener, allows EventListener to be removed inside of a EventListener
+		/// </summary>
+		internal void InvokeAll()
+		{
+			currentlyIterating = true;
+
+			try {
+				_eventListeners.ForEach(
+					(partEventListener =>
+					{
+						if (!partEventListener.delete) {
+							partEventListener.action.Invoke();
+						}
+					})
+				);
+			} catch {
+				currentlyIterating = false;
+			}
+
+			currentlyIterating = false;
+
+			for (int i = _eventListeners.Count - 1; i >= 0; i--) {
+				if (_eventListeners[i].delete) {
+					_eventListeners.RemoveAt(i);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Returns if the collection contains a specific EventListener
+		/// </summary>
+		/// <param name="eventListener">The EventListener object to check</param>
+		/// <returns>True if the collection contains the provided EventListener, otherwise false</returns>
+		internal bool Contains(GlobalEventListener eventListener)
+		{
+			return _eventListeners.Contains(eventListener);
+		}
+
+		/// <summary>
+		/// Removes an EventListener object from the collection
+		/// If the InvokeAll is currently being run, the EventListener will not be executed (if it hasn't already) and removed after the InvokeAll is finished
+		/// </summary>
+		/// <param name="eventListener">The EventListener to remove</param>
+		/// <returns>True if the EventListener was removed (or will be removed)</returns>
+		internal bool Remove(GlobalEventListener eventListener)
+		{
+			if (!Contains(eventListener)) {
+				return false;
+			}
+
+			if (!currentlyIterating) {
+				return _eventListeners.Remove(eventListener);
+			}
+
+			eventListener.delete = true;
+			return true;
+		}
+	}
+}
---
+++ src/MwcModApi/EventSystem/GlobalEvents.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using MwcModApi.Caching;
+
+namespace MwcModApi.EventSystem
+{
+	public class GlobalEvents
+	{
+		protected static GlobalEvents instance;
+
+		/// <summary>
+		/// Stores all events that a developer may have added to this part object
+		/// </summary>
+		internal Dictionary<GlobalEventType, GlobalEventListenerCollection> events = new Dictionary<GlobalEventType, GlobalEventListenerCollection>();
+
+		internal GlobalEvents()
+		{
+			foreach (GlobalEventType eventType in Enum.GetValues(typeof(GlobalEventType)))
+			{
+				events.Add(eventType, new GlobalEventListenerCollection());
+			}
+		}
+
+		public static GlobalEvents GetInstance()
+		{
+			return instance ?? (instance = new GlobalEvents());
+		}
+
+		/// <summary>
+		/// Add a global event listener and react to this event being triggered
+		/// </summary>
+		/// <param name="type">Type of event you want to listen to</param>
+		/// <param name="action">The code you want to execute on event trigger</param>
+		/// <param name="invokeActionIfConditionMet">Immediately execute the action if it's condition is met</param>
+		/// <returns>a reference to a GlobalEventListener object, used for later removal</returns>
+		public GlobalEventListener AddEventListener(
+			GlobalEventType type,
+			Action action,
+			bool invokeActionIfConditionMet = true
+		)
+		{
+			GlobalEventListener partEventListener = new GlobalEventListener(type, action);
+			events[type].Add(partEventListener);
+
+			if (!invokeActionIfConditionMet) {
+				return partEventListener;
+			}
+
+			switch (type) {
+				case GlobalEventType.EngineRunning:
+					if (CarH.running) {
+						action.Invoke();
+					}
+					break;
+				case GlobalEventType.EngineStalled:
+					if (!CarH.running) {
+						action.Invoke();
+					}
+					break;
+			}
+
+			return partEventListener;
+		}
+
+		/// <summary>
+		/// Remove a previously added event listener again
+		/// </summary>
+		/// <param name="eventListener">The event listener to remove</param>
+		/// <returns>true on success, otherwise false</returns>
+		public bool RemoveEventListener(GlobalEventListener eventListener)
+		{
+			var collection = GetEventListeners(eventListener.type);
+			return collection.Contains(eventListener) && collection.Remove(eventListener);
+		}
+
+		internal GlobalEventListenerCollection GetEventListeners(GlobalEventType type)
+		{
+			return events[type];
+		}
+
+		/// <summary>
+		/// Called when the MwcModApi mod loads to cleanup static data
+		/// </summary>
+		internal static void LoadCleanup()
+		{
+			instance = null;
+		}
+	}
+}
